6b855d36490a0729b55391f699914159364775d2
lamie419 <132032420+lamie419@users.noreply.github.com>
2023-06-03 16:03:43 +0800

    website

---
+++ website/home.js
@@ -0,0 +1,16 @@
+var btn = $('.floating-text');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 30) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '30');
+});
+
+console.log(p)
